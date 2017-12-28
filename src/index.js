834a7ecb186e1011e1f78ab89c1cf16155a5724b
Mark Hernandez <markedcode@gmail.com>
2017-12-28 13:12:03 -0600

    Establish client JSON post to server

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ import React from 'react'
 import { render } from 'react-dom'
 import { App } from './app'
 import { isAuth } from './api'
+import { dataFetch } from './utils'
 
 render(<App />, document.getElementById('root'))
 console.log('Index loaded')
 
 isAuth().then(console.log)
+
+dataFetch('/api/blogs', { msg: 'hello' }, { response: 'nothing' }).then(
+  console.log
+)
--- src/utils.js
+++ src/utils.js
@@ -10,9 +10,40 @@ export const apiFetch = async url => {
   }
 }
 
+export const apiRequest = async (url, body) => {
+  try {
+    return await (await window.fetch(url, {
+      body,
+      credentials: 'same-origin',
+      headers: {
+        'content-type': 'application/json'
+      },
+      method: 'post'
+    })).json()
+  } catch (e) {
+    throw e
+  }
+}
+
 export const dataGrab = async (url, defaultData = null) => {
   try {
     const data = await apiFetch(url)
+    const { error } = data
+
+    if (error) {
+      throw new Error(error)
+    } else {
+      return data
+    }
+  } catch (e) {
+    errorCatch(e)
+    return defaultData
+  }
+}
+
+export const dataFetch = async (url, body = {}, defaultData = null) => {
+  try {
+    const data = await apiRequest(url, JSON.stringify(body))
     const { error } = data
 
     if (error) {
