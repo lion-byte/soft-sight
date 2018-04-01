daff4ef16d858c7b41763f1504df05b8a95d82c9
Mark Hernandez <markedcode@gmail.com>
2018-03-31 20:06:39 -0500

    Now! Fixes path error

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,8 +1,11 @@
 import { create } from 'axios'
+import { join } from 'path'
 
 export const instance = create({
   baseURL:
-    process.env.NODE_ENV === 'production' ? '/.netlify/functions/api' : '/api',
+    process.env.NODE_ENV === 'production'
+      ? join(window.location.toString(), '/.netlify/functions/api')
+      : '/api',
   timeout: 10000
 })
 
