42ca1241c2ec807fe0fc28c71d5b0f068696876f
Mark Hernandez <MH81796@gmail.com>
2018-04-07 09:30:52 -0500

    Add contact links (#14)
    * Resolves #11

---
+++ src/pages/contact.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react'
+
+const ContactPage = () => (
+  <Fragment>
+    <section className='splash'>
+      <h2>Contact</h2>
+    </section>
+
+    <p>
+      Feature requests and questions can be sent to{' '}
+      <a href='mailto:mark@lion-byte.com'>mark@lion-byte.com</a>.
+    </p>
+  </Fragment>
+)
+
+export default ContactPage
