cb4242066d991a5846c6ce31a4fa6317f625ee44
Mark Hernandez <mark@lion-byte.com>
2020-01-19 13:05:52 -0600

    Add react-hook-form

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,10 @@
-import React, { PureComponent } from 'react'
+import React, { useState, useCallback } from 'react'
 import styled from 'styled-components'
+import { useForm } from 'react-hook-form'
 
 import Blog from './Blog'
 
-const SearchStyles = styled.section`
+const SearchStyles = styled.div`
   form {
     display: flex;
     flex-direction: column;
@@ -19,6 +20,7 @@ const SearchStyles = styled.section`
       align-items: center;
       display: flex;
       flex-direction: row;
+      font-style: italic;
       justify-content: space-between;
     }
   }
@@ -28,82 +30,64 @@ const SearchStyles = styled.section`
   }
 `
 
-export class Search extends PureComponent {
-  constructor (props) {
-    super(props)
+export function Search () {
+  /**
+   * @typedef {ReadonlyArray<string>} SearchList
+   */
+  /**
+   * @type {[SearchList, React.Dispatch<React.SetStateAction<SearchList>>]}
+   */
+  const [prevSearches, setPrevSearches] = useState([])
+  const { handleSubmit, register, reset } = useForm()
 
-    this.state = {
-      prevSearches: [],
-      text: ''
-    }
+  const onSubmit = useCallback(
+    /**
+     * @param {Record<string, string>} data
+     */
+    data => {
+      const blogName = (data.blogName || '')
+        .trim()
+        .replace(/ /g, '-')
+        .toLowerCase()
 
-    this.handleChange = this.handleChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
-  }
-
-  handleChange (e) {
-    this.setState({ text: e.target.value })
-  }
-
-  handleSubmit (e) {
-    e.preventDefault()
-
-    /* Trims surrounding whitespace and replaces inner-spaces with hyphens */
-    const text = this.state.text
-      .trim()
-      .split(' ')
-      .join('-')
-
-    if (text === '') {
-      return
-    }
-
-    this.setState(prevState => {
-      if (prevState.prevSearches.includes(text)) {
-        return {}
+      if (!blogName || prevSearches.includes(blogName)) {
+        return
       }
 
-      return {
-        prevSearches: [text, ...prevState.prevSearches],
-        text: ''
-      }
-    })
-  }
+      setPrevSearches([blogName].concat(prevSearches))
+      reset()
+    },
+    [prevSearches, reset]
+  )
 
-  render () {
-    const { prevSearches, text } = this.state
+  return (
+    <SearchStyles>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label htmlFor='search-input'>Tumblr Username</label>
+        <input
+          id='search-input'
+          type='text'
+          placeholder='Check if Tumblr blog is explicit'
+          name='blogName'
+          autoComplete='off'
+          ref={register}
+          required
+        />
 
-    return (
-      <SearchStyles>
-        <form onSubmit={this.handleSubmit}>
-          <label htmlFor='search-input'>Tumblr Username</label>
-          <input
-            id='search-input'
-            type='text'
-            onChange={this.handleChange}
-            placeholder='Check if Tumblr blog is explicit'
-            value={text}
-          />
+        <div className='options'>
+          <p>Previous search results will be shown below the current search.</p>
 
-          <div className='options'>
-            <p>
-              <i>
-                Previous search results will be shown below the current search.
-              </i>
-            </p>
+          <button type='submit'>Check</button>
+        </div>
+      </form>
 
-            <button type='submit'>Check</button>
-          </div>
-        </form>
-
-        <section className='results'>
-          {prevSearches.map(name => (
-            <Blog key={name} blogName={name} />
-          ))}
-        </section>
-      </SearchStyles>
-    )
-  }
+      <section className='results'>
+        {prevSearches.map(name => (
+          <Blog key={name} blogName={name} />
+        ))}
+      </section>
+    </SearchStyles>
+  )
 }
 
 export default Search
